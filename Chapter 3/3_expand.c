de1c431cced2bd48c86c6900d21859555e1aa275
uitxj <uitxj@student.kit.edu>
2019-04-01 14:40:33 +0200

    commiting a lot scared of crash

---
+++ Chapter 3/3_expand.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int expand(char s[], char t[]);
+
+int main(){
+    char s[] = "a-z blabla hello my name is";
+    char t[100];
+
+}
+
+int expand(char s[], char t[]){
+
+}
