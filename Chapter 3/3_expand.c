2de19c99d719c842bb1fcfd74bbaaac14e3b1d50
uitxj <uitxj@student.kit.edu>
2019-04-01 14:46:02 +0200

    commiting a lot scared of crash

--- Chapter 3/3_expand.c
+++ Chapter 3/3_expand.c
@@ -10,5 +10,17 @@ int main(){
 }
 
 int expand(char s[], char t[]){
+    int i;
 
+    for(i = 0; s[i] != '\0'; i++){
+        if(s[i] == '-' ){
+            switch(s[--i]){
+                case ((s[i] >= 'a') && (s[i] <= 'z')):
+                    for(j = i; j != ('z' + 1); j++){
+                        t[j] =
+                    }
+                    break;
+            }
+        }
+    }
 }
