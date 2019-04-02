5b5941c2a628041a6d5c92493ae8995403f82c48
uitxj <uitxj@student.kit.edu>
2019-04-02 19:26:50 +0200

    last commit probably, might work on book after exam

--- Chapter 5/5_pointers.c
+++ Chapter 5/5_pointers.c
@@ -9,5 +9,15 @@ int main(){
     int *daytab[13];
     daytab[0] = ptr;
 
+    struct element{
+        int value;
+        int key;
+        char used;
+    };
+
+    struct element table[10];
+    for(int i = 0; i < 10; i++)
+        printf("%c\n", table[i].used);
+
 
 }
